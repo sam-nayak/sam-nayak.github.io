42eabf5622e5f99dbbe3bfb28ffdde7af7340de5
Sam <samnayak@live.co.uk>
2021-08-01 21:58:31 +0100

    Simple clock.

--- assets/js/index.js
+++ assets/js/index.js
@@ -86,27 +86,25 @@ Number.prototype.pad = function(n) {
   return r;
 };
 
-function updateClock() {
-  var now = new Date();
-  var milli = now.getMilliseconds(),
-    sec = now.getSeconds(),
-    min = now.getMinutes(),
-    hou = now.getHours(),
-    mo = now.getMonth(),
-    dy = now.getDate(),
-    yr = now.getFullYear();
-  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var tags = ["mon", "d", "y", "h", "m", "s", "mi"],
-    corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), milli];
-  for (var i = 0; i < tags.length; i++)
-    document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+function clock() {// We create a new Date object and assign it to a variable called "time".
+var time = new Date(),
+
+    // Access the "getHours" method on the Date object with the dot accessor.
+    hours = time.getHours(),
+
+    // Access the "getMinutes" method with the dot accessor.
+    minutes = time.getMinutes(),
+
+
+    seconds = time.getSeconds();
+
+document.querySelectorAll('.clock')[0].innerHTML = harold(hours) + ":" + harold(minutes) + ":" + harold(seconds);
+
+  function harold(standIn) {
+    if (standIn < 10) {
+      standIn = '0' + standIn
+    }
+    return standIn;
+  }
 }
-
-function initClock() {
-  updateClock();
-  window.setInterval("updateClock()", 1);
-}
-
-// END CLOCK SCRIPT
-
-initClock();
+setInterval(clock, 1000);
